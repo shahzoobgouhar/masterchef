26520dce440df45bb68d1a436a582226612ece47
shahzoob <shaaz465@gmail.com>
2020-12-31 01:26:04 +0530

    observable project code

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -26,7 +26,6 @@ export class RecipeService {
         )
 
     ];
-    public selectedRecipe = new EventEmitter<Recipe>()
 
     constructor(private shoppingListService: ShoppingListService){}
     getRecipes() {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-    ingredientChanged = new EventEmitter<Ingredient[]>()
+    ingredientChanged = new Subject<Ingredient[]>()
     public ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 5),
@@ -13,10 +13,10 @@ export class ShoppingListService {
     }
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
     }
     addIngredients(ingredients: Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
     }
 }
