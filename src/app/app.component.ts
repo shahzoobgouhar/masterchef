e1a9f8055b04ef2ff8d1224f642f8a7fbd3ccb43
shahzoob <shaaz465@gmail.com>
2020-12-31 00:58:16 +0530

    observable code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  isActivated: boolean = false;
+  private activatedSub: Subscription
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe((activate: boolean)=>{
+      this.isActivated = activate;
+    })
+  }
+
+  ngOnDestroy(){
+    this.activatedSub.unsubscribe()
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  counter: Observable<number>
+  counterCubscription: Subscription
   constructor() { }
 
   ngOnInit() {
+    // this.counter = interval(1000);
+
+    this.counter = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        if (count === 2) {
+          observer.complete()
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'))
+        }
+        observer.next(count);
+        count++
+      }, 1000)
+    })
+
+    this.counterCubscription = this.counter.pipe(filter(data=>{
+      return data > 0
+    }), map((data) => {
+      return `Round ${data+1}`;
+    })).subscribe((count) => {
+      console.log(count);
+    }, (error) => {
+      console.log(error);
+    }, () => {
+      console.log('completed!!');
+    })
   }
 
+  ngOnDestroy() {
+    this.counterCubscription.unsubscribe()
+  }
 }
